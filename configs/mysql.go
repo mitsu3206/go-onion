7cc15bf58cae81d0b72b4e6bb9dd2b0f0855af55
mitsu3206 <kohshelltoh3206@gmail.com>
2024-05-29 23:50:48 +0900

    update: Added error output when 'env' is not set

--- configs/mysql.go
+++ configs/mysql.go
@@ -11,17 +11,25 @@ import (
 func GetMysqlConfig() *mysql.Config {
 	jst, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	c := &mysql.Config{
-		User:      os.Getenv("DB_USER"),
-		Passwd:    os.Getenv("DB_PASS"),
+		User:      findEnv("DB_USER"),
+		Passwd:    findEnv("DB_PASS"),
 		Net:       "tcp",
-		Addr:      os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT"),
-		DBName:    os.Getenv("DB_NAME"),
+		Addr:      findEnv("DB_HOST") + ":" + findEnv("DB_PORT"),
+		DBName:    findEnv("DB_NAME"),
 		ParseTime: true,
 		Collation: "utf8mb4_general_ci",
 		Loc:       jst,
 	}
 	return c
+}
+
+func findEnv(key string) string {
+	env, err := os.LookupEnv(key)
+	if !err {
+		log.Printf("error: Failed to locate the 'env' for key = %s", key)
+	}
+	return env
 }
